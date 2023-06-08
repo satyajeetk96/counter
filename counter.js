198c4f8503c9f739822753a296c68f38e67026c5
Satyajeet Kumar <84793523+satyajeetk96@users.noreply.github.com>
2023-06-08 14:38:20 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,52 @@
+const btn1 = document.getElementById("dec");
+const btn2 = document.getElementById("res");
+const btn3 = document.getElementById("inc");
+
+var number = 0;
+const num = document.querySelector("span");
+
+btn1.addEventListener("click", function(){
+    number-=1;
+    if(number<0){
+        num.style.color = "red";
+    }
+    else if(number>0){
+          num.style.color = "#7fff00"
+    }
+    else{
+        num.style.color = "#29d2c7"
+    }
+    num.innerText= number;
+
+})
+
+btn2.addEventListener("click", function(){
+    number = 0;
+    if(number<0){
+        num.style.color = "red";
+    }
+    else if(number>0){
+          num.style.color = "#7fff00"
+    }
+    else{
+        num.style.color = "#29d2c7"
+    }
+    num.innerText = number;
+
+})
+
+btn3.addEventListener("click", function(){
+    number+=1;
+    if(number<0){
+        num.style.color = "red";
+    }
+    else if(number>0){
+          num.style.color = "#7fff00"
+    }
+    else{
+        num.style.color = "#29d2c7"
+    }
+    num.innerText= number;
+
+})
+
